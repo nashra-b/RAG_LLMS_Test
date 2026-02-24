b2e90ff2bb78391272aee24740949adbd1b8a28e
Nashra Babar <nashrababar111@gmail.com>
2026-02-24 17:13:57 -0500

    Add FastAPI server implementation with Langchain integration

---
+++ serve.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os 
+from dotenv import load_dotenv
+from langserve import add_routes   
+
+load_dotenv()
+groq_api_key=os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="llama-3.1-8b-instant",groq_api_key=groq_api_key)
+
+## 1. Creating the prompt Template : 
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+parser=StrOutputParser()
+
+## create chain 
+chain = prompt_template| model |parser 
+
+## app definition 
+app = FastAPI(
+    title="Langchain API",
+    description="Simple API  server for translating text",
+    version="1.0.0" 
+)
+## adding the chain routes to the app :
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__ == "__main__":
+    import uvicorn 
+    uvicorn.run(app, host="localhost", port=8000)
